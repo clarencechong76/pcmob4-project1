8be9113e51f87a5468afd2c22edfe7248eac0d50
Admin <mobiletime@Clarence.local>
2021-05-20 21:56:58 +0800

    add arrival state

--- App.js
+++ App.js
@@ -1,12 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [loading, setloading] = useState(true); 
+  const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
+
+function loadBusStopData() {
+fetch(BUSSTOP_URL)
+.then((Response) => {
+  return Response.json();
+})
+.then((ResponseData) => {
+  console.log("Original data");
+
+  const myBus = ResponseData.services.filter(
+    (item) => item.no === "155"
+  )[0];
+  console.log("My bus");
+  console.log(myBus);
+
+});
+
+}
+useEffect(() => {
+  loadBusStopData();
+}, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Bus arrival time</Text>
+      <Text style={styles.arrivalTime}>
+      {loading ? <ActivityIndicator size="large" /> : "Loaded"}
+      </Text>
+      <TouchableOpacity style={styles.button}>
+<Text style={styles.buttonText}>Refresh!</Text>
+
+      </TouchableOpacity>
+      
     </View>
   );
 }
@@ -18,4 +49,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    backgroundColor: 'green',
+
+    padding: 20,
+    borderRadius: 10,
+    marginTop: 20,
+  }
 });
